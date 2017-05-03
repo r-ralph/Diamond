5d6f8b8d31069bd3e5e19110dacd175f66111633
Ralph <git@ralph.ms>
2017-05-04 01:54:44 +0900

    Add child projects

---
+++ Diamond-Demo-UWP/App.xaml.cs
@@ -0,0 +1,108 @@
+﻿#region Copyright
+
+// Copyright 2017 Ralph (Tamaki Hidetsugu)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Diamond.Demo.UWP
+{
+    /// <summary>
+    /// 既定の Application クラスを補完するアプリケーション固有の動作を提供します。
+    /// </summary>
+    sealed partial class App
+    {
+        /// <summary>
+        /// 単一アプリケーション オブジェクトを初期化します。これは、実行される作成したコードの
+        ///最初の行であるため、main() または WinMain() と論理的に等価です。
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// アプリケーションがエンド ユーザーによって正常に起動されたときに呼び出されます。他のエントリ ポイントは、
+        /// アプリケーションが特定のファイルを開くために起動されたときなどに使用されます。
+        /// </summary>
+        /// <param name="e">起動の要求とプロセスの詳細を表示します。</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            // ウィンドウに既にコンテンツが表示されている場合は、アプリケーションの初期化を繰り返さずに、
+            // ウィンドウがアクティブであることだけを確認してください
+            if (rootFrame == null)
+            {
+                // ナビゲーション コンテキストとして動作するフレームを作成し、最初のページに移動します
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: 以前中断したアプリケーションから状態を読み込みます
+                }
+
+                // フレームを現在のウィンドウに配置します
+                Window.Current.Content = rootFrame;
+            }
+
+            if (!e.PrelaunchActivated)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // ナビゲーション スタックが復元されない場合は、最初のページに移動します。
+                    // このとき、必要な情報をナビゲーション パラメーターとして渡して、新しいページを
+                    //構成します
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // 現在のウィンドウがアクティブであることを確認します
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// 特定のページへの移動が失敗したときに呼び出されます
+        /// </summary>
+        /// <param name="sender">移動に失敗したフレーム</param>
+        /// <param name="e">ナビゲーション エラーの詳細</param>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// アプリケーションの実行が中断されたときに呼び出されます。
+        /// アプリケーションが終了されるか、メモリの内容がそのままで再開されるかに
+        /// かかわらず、アプリケーションの状態が保存されます。
+        /// </summary>
+        /// <param name="sender">中断要求の送信元。</param>
+        /// <param name="e">中断要求の詳細。</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: アプリケーションの状態を保存してバックグラウンドの動作があれば停止します
+            deferral.Complete();
+        }
+    }
+}
