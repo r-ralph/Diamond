719fa90b8418915690d77d17028a36979ef509d4
Ralph <git@ralph.ms>
2017-05-05 12:55:39 +0900

    Add demo project of .NET Core console app

---
+++ Diamond-Demo-NETCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Diamond.Demo.NETCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(typeof(World.World));
+            Console.ReadKey(true);
+        }
+    }
+}
