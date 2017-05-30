660181411526940ff5b03bfa365994966ea56968
Ralph <git@ralph.ms>
2017-05-31 03:13:42 +0900

    Fix namespace

---
+++ Diamond-Provider-Remote/RemoteWorld.cs
@@ -0,0 +1,42 @@
+﻿#region Copyright
+
+// Copyright 2017 Ralph (Tamaki Hidetsugu)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+namespace Diamond
+{
+    public class RemoteWorld : World
+    {
+        private RemoteWorld()
+        {
+        }
+
+        public override Block GetBlock(int x, int y, int z)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Chunk GetChunk(int cx, int cz)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsBlockAvailavle(Block block)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
